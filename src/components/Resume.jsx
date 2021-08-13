1c426cefd18b99801318c59d54175a8013208388
Megan Zinka <meganzinka@gmail.com>
2021-08-13 13:28:02 -0400

    final touches

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,11 +7,9 @@ import PDFViewer from "pdf-viewer-reactjs";
 const Resume = () => {
   return (
     <div>
-      <div id="resume-container">
       <div id ="resume-container">
 			<embed src= "./MeganZinkaResume.pdf" type="application/pdf" width="100%" height="800px"/>
 		</div>
-      </div>
       <div id="resume-container-mobile">
         <PDFViewer
           id="resume-pdf"
