2b84d0d9fe5ddc2feaeae7a3d92d69cd3d079ac6
meganzinka <meganzinka@gmail.com>
2021-09-07 16:30:21 -0400

    changed about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,8 +18,6 @@ const About = () => {
           I’ve always loved math. As a Math for America/Noyce fellow, I spent the past six years trying to show students in Boston Public Schools how cool and empowering it can be, but now I’m so excited to be in the tech field! When I was teaching, I spent my nights learning how to code, but this year I took the plunge and completed an intensive software development bootcamp. I find my background in mathematics and statistics not only to be incredibly useful in problem solving, I am revelling in the opportunity to think critically, logically, and mathematically in new and exciting ways. As a teacher, I constantly thought about how to make math content accessible - minimizing clicks and steps to getting to the core of the lesson - and I have found the design instincts I developed in my teaching career to be tremendously useful as I think about user experience and interface in web development. The more I learn, the more I find new interests and passions, like sleek design, intuitive systems, and efficiency.
          <br></br>
          <br></br>
- 
-         After completing the bootcamp, I spent a month in Brazil on a film crew as a gaffer (the person who does lighting). The documentary features women who have transformed their lives, families and communities through entrepreneurship, and being a part of the crew was one of the most incredible experiences of my life. Now that I'm back, I'm excited to enter the tech field and continue learning, growing, and creating!
        </div>
 
 
